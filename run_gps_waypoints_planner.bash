7baae206d61b7beab10285ffd8817c0517772b0a
zeki411 <nguyenhatrung411@gmail.com>
2024-01-17 13:51:00 +0000

    first commit

---
+++ run_gps_waypoints_planner.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /workspaces/simtb3_demo/simtb3_ws
+source /opt/ros/iron/setup.bash
+source install/setup.bash
+
+ros2 launch simtb3 simtb3.launch.py
--- setup.bash
+++ setup.bash
@@ -1,3 +1,22 @@
-mkdir -p simtb3_ws/src
-cd simtb3_ws/src
+#!/bin/bash
+
+# Install necessary packages
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-tk
+
+# Install Gazebo
+curl -sSL http://get.gazebosim.org | sh
+
+# Create a workspace
+mkdir -p /workspaces/simtb3_demo/simtb3_ws/src
+cd /workspaces/simtb3_demo/simtb3_ws/src
 git clone https://github.com/serene4uto/simtb3.git
+cd ..
+
+# Install ROS2 dependencies
+source /opt/ros/iron/setup.bash
+rosdep install -y --from-paths src --ignore-src
+
+# Build the source code
+colcon build --symlink-install
